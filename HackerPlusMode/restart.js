58fd89eb41e40f8f4be5307997691deefc7b6a2c
Crimson-ray <155520947+Crimson-ray@users.noreply.github.com>
2024-06-06 19:21:58 +0530

    Update restart.js

--- HackerPlusMode/restart.js
+++ HackerPlusMode/restart.js
@@ -24,9 +24,9 @@ let shufflepiece=[
     '','','','','','','','',
     '','','',semi,ricochet,tank,titan
 ]
-const shootSound = new Audio("canon-sound.mp3");
+const shootSound = new Audio("https://github.com/Crimson-ray/Delta-Task1/assets/155520947/4fbbe134-c756-44b9-bb76-26c31c4b739a");
 shootSound.preload="auto"
-const winSound = new Audio("game-end.mp3");
+const winSound = new Audio("https://github.com/Crimson-ray/Delta-Task1/assets/155520947/daf60c41-c97d-4c5c-9aca-ffca6b5c1f79");
 const historyA=document.querySelector(".historyA")
 const historyB=document.querySelector(".historyB")
 let movesOfA=[]
